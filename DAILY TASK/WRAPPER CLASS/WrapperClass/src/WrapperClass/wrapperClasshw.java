85c2e5bc2b94f2507c0349605a4bfe75bffa839e
SWETHASHRILNR <swethaandswe@gmail.com>
2024-12-19 11:29:28 +0530

    Add files via upload

---
+++ DAILY TASK/WRAPPER CLASS/WrapperClass/src/WrapperClass/wrapperClasshw.java
@@ -0,0 +1,13 @@
+package WrapperClass;
+public class wrapperClasshw 
+{
+	public static void main(String[] args) 
+	{
+		int i = 20;
+		boolean b = true;
+		Integer intObject = i;
+		Boolean boolObject  = b;
+		System.out.println("Integer Object: "+intObject);
+		System.out.println("Boolean Object: "+boolObject);
+	}
+}
---
+++ DAILY TASK/WRAPPER CLASS/WrapperClass/src/WrapperClass/wrapperUnboxing.java
@@ -0,0 +1,11 @@
+package WrapperClass;
+public class wrapperUnboxing 
+{
+	public static void main(String[] args) 
+	{
+        Integer i = new Integer(10);
+        int i1 = i;
+        System.out.println("Value of i:" + i);
+        System.out.println("Value of i1: " + i1);
+	}
+}
